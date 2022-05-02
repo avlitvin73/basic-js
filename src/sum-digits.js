2cec70d5a97117c62f45208a9e052a47533d846b
av.litvin <av.litvin73@gmail.com>
2022-05-03 01:59:25 +0300

    feat: sum

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  num = Math.abs(n)
+  let lastNum
+  let output = 0
+  while (num > 0) {
+    lastNum = num % 10
+    output += lastNum 
+    num = Math.floor(num / 10)
+  }
+  if (output > 9) {
+    return getSumOfDigits(output)
+  }
+  return output
+  
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -14,7 +14,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  throw new NotImplementedError('Not implemented');
   if(!Array.isArray(arr)){
     throw new Error(`'arr' parameter must be an instance of the Array!`)
   }
